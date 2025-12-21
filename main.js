a88f52247c14aa8615db6f1447b0bb2e966a9388
Carlos Carrillo B <ccarrillo29@gmail.com>
2025-12-21 19:34:31 -0300

    Colaboración Carlos Carrillo Bastidas

--- main.js
+++ main.js
@@ -43,3 +43,48 @@ app.listen(PORT,
 
 
 // app.delete("/canciones")
+
+// Inicio actividad por Carlos Carrillo Bastidas
+
+app.post("/canciones", (req, res) => {
+    const canciones = JSON.parse(
+        fs.readFileSync("./data/canciones.json", "utf8")
+    );
+    canciones.push(req.body);
+    
+    fs.writeFileSync("./data/canciones.json", JSON.stringify(canciones, null, 2));
+    res.send("agregado muchachon");
+});
+
+app.put("/canciones/:id", (req, res) => {
+    const canciones = JSON.parse(
+        fs.readFileSync("./data/canciones.json", "utf8")
+    );
+    const id = parseInt(req.params.id);
+    const index = canciones.findIndex((cancion) => cancion.id === id);
+    if (index !== -1) {
+        canciones[index] = { ...canciones[index], ...req.body };
+        fs.writeFileSync("./data/canciones.json", JSON.stringify(canciones, null, 2));
+        res.send("cancion actualizada");
+    } else {
+        res.status(404).send("cancion no encontrada");
+    }   
+});
+
+app.delete("/canciones/:id", (req, res) => {
+    const canciones = JSON.parse(
+        fs.readFileSync("./data/canciones.json", "utf8")
+    );
+    const id = parseInt(req.params.id);
+    const index = canciones.findIndex((cancion) => cancion.id === id);
+    if (index !== -1) {
+        canciones.splice(index, 1);
+        fs.writeFileSync("./data/canciones.json", JSON.stringify(canciones, null, 2));
+        res.send("cancion eliminada");
+    } else {
+        res.status(404).send("cancion no encontrada");
+    }   
+}
+);
+
+// Fin actividad por Carlos Carrillo Bastidas
